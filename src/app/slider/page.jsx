05959f41f21f58f496f655dcf721cb984e90cafb
b4yrra <t.chinbayr@gmail.com>
2026-01-09 10:47:27 +0800

    a

--- src/app/slider/page.jsx
+++ src/app/slider/page.jsx
@@ -4,11 +4,18 @@ import { useState } from "react";
 const person = [
   {
     id: 1,
+    firstName: "John",
   },
 ];
 
 export default function Home() {
   const [review, setReview] = useState(person);
 
-  return <div>hello</div>;
+  return (
+    <div>
+      {review.map((r) => {
+        return <p>{r.firstName}</p>;
+      })}
+    </div>
+  );
 }
